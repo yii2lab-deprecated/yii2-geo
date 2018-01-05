43a56abec00a2117ed6d69ab04c2a9d9b820919a
Vitaliy <theyamshikov@yandex.ru>
2018-01-05 21:05:42 +0600

    fix method

--- src/api/helpers/GeoHelper.php
+++ src/api/helpers/GeoHelper.php
@@ -3,15 +3,20 @@
 namespace yii2lab\geo\api\helpers;
 
 use Yii;
+use yii2lab\domain\data\Query;
 
 /* todo: перенести в модуль транзакций на фронтэнд */
 
 class GeoHelper {
-	public static function getCites() {
-		$cites = Yii::$app->geo->city->all();
+	public static function getCites($countryId = null) {
+		$query = Query::forge();
+		if($countryId) {
+			$query->where('country_id', $countryId);
+		}
+		$cites = Yii::$app->geo->city->all($query);
 		$arrayCites = [null => 'все города'];
 		foreach($cites as $city) {
-			$arrayCites[ $city->id ] = $city->city_name;
+			$arrayCites[ $city->id ] = $city->name;
 		}
 		return $arrayCites;
 	}
