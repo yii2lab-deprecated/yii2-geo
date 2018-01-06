a8261578a09a354f4691b9a52f8c7853e50c14fa
Vitaliy <theyamshikov@yandex.ru>
2018-01-06 21:29:48 +0600

    refacto widget

--- src/widgets/GeoSelector.php
+++ src/widgets/GeoSelector.php
@@ -6,12 +6,12 @@ use Yii;
 use yii\base\Widget;
 use yii2lab\domain\data\Query;
 use yii\helpers\ArrayHelper;
+use yii2lab\widgets\ajaxSelector\AjaxSelector;
 
 class GeoSelector extends Widget
 {
 	
 	public $form;
-	public $formId;
 	public $model;
 	public $default = [];
 	
@@ -20,43 +20,51 @@ class GeoSelector extends Widget
 	 */
 	public function run()
 	{
-		$country = $this->getCountryCollection();
-		$region = $this->getRegionCollection(1894);
-		$city = $this->getCityCollection($this->model->region_id);
-		return $this->render('fields', [
+		//$this->formId = strtolower(basename(get_class($this->model)));
+		$entities = [
+			'country' => [
+				//'primaryKey' => 'id',
+				//'elementName' => 'country_id',
+				//'elementId' => $this->formId . '-country_id',
+				'uri' => 'v4/country',
+				'prompt' => Yii::t('geo/main', 'select_country'),
+				'childName' => 'region',
+				'options' => $this->getCollection('country'),
+			],
+			'region' => [
+				//'primaryKey' => 'id',
+				//'elementName' => 'region_id',
+				//'elementId' => $this->formId . '-region_id',
+				'uri' => 'v4/region',
+				'prompt' => Yii::t('geo/main', 'select_region'),
+				'childName' => 'city',
+				'options' => $this->getCollection('region', 'country_id'),
+			],
+			'city' => [
+				//'primaryKey' => 'id',
+				//'elementName' => 'city_id',
+				//'elementId' => $this->formId . '-city_id',
+				'uri' => 'v4/city',
+				'prompt' => Yii::t('geo/main', 'select_city'),
+				'childName' => null,
+				'options' => $this->getCollection('city', 'region_id'),
+			],
+		];
+		
+		return AjaxSelector::widget([
 			'form' => $this->form,
-			'formId' => $this->formId,
 			'model' => $this->model,
-			'country' => $country,
-			'region' => $region,
-			'city' => $city,
+			'entities' => $entities,
 		]);
 	}
 	
-	private function getCountryCollection() {
+	private function getCollection($entityName, $fieldName = null) {
 		$query = Query::forge();
-		$query->orderBy('name');
-		$collection = Yii::$app->geo->country->all($query);
-		$collection = ArrayHelper::map($collection, 'id', 'name');
-		return $collection;
-	}
-	
-	private function getRegionCollection($countryId) {
-		$query = Query::forge();
-		$query->where('country_id', $countryId);
-		//$query->perPage(9999999);
-		$query->orderBy('name');
-		$collection = Yii::$app->geo->region->all($query);
-		$collection = ArrayHelper::map($collection, 'id', 'name');
-		return $collection;
-	}
-	
-	private function getCityCollection($regionId) {
-		$query = Query::forge();
-		$query->where('region_id', $regionId);
-		//$query->perPage(9999999);
-		$query->orderBy('name');
-		$collection = Yii::$app->geo->city->all($query);
+		if(!empty($fieldName)) {
+			$value = $this->model->hasProperty($fieldName) ? $this->model->{$fieldName} : ArrayHelper::getValue($this->default, $fieldName);
+			$query->where($fieldName, $value);
+		}
+		$collection = Yii::$app->geo->{$entityName}->all($query);
 		$collection = ArrayHelper::map($collection, 'id', 'name');
 		return $collection;
 	}
