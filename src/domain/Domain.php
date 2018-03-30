b06eabf9d563424435b3a848926a3838e0434904
Vitaliy <theyamshikov@yandex.ru>
2018-03-30 15:29:16 +0600

    make docblock in domain

--- src/domain/Domain.php
+++ src/domain/Domain.php
@@ -2,6 +2,17 @@
 
 namespace yii2lab\geo\domain;
 
+/**
+ * Class Domain
+ *
+ * @package yii2lab\geo\domain
+ *
+ * @property \yii2lab\geo\domain\services\RegionService $region
+ * @property \yii2lab\geo\domain\services\CityService $city
+ * @property \yii2lab\geo\domain\services\CountryService $country
+ * @property \yii2lab\geo\domain\services\CurrencyService $currency
+ *
+ */
 class Domain extends \yii2lab\domain\Domain {
 	
 	public function config() {
